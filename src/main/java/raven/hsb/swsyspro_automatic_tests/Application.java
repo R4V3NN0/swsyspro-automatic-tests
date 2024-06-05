8c1bae2bc9fe0bd41129991e76b19d95d1cff78c
Noah Raven <raven.noah@gmx.de>
2024-06-05 14:26:33 +0200

    Spring init

---
+++ src/main/java/raven/hsb/swsyspro_automatic_tests/Application.java
@@ -0,0 +1,13 @@
+package raven.hsb.swsyspro_automatic_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/raven/hsb/swsyspro_automatic_tests/ApplicationTests.java
@@ -0,0 +1,13 @@
+package raven.hsb.swsyspro_automatic_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
